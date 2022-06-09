28122f0c8448bb34aff2eb13f3f67adf73468bd8
unknown <nicolas.degouveia@gmail.com>
2022-06-09 22:44:54 +0200

    roadmap added

--- src/Roadmap/index.js
+++ src/Roadmap/index.js
@@ -1,9 +1,13 @@
 import './Styles.scss';
+import road from '../assets/map.png'
 
 export default function Roadmap() {
   return (
     <div className="road">
       <p>ROADMAP</p>
+      <div className='road__image'>
+        <img src={road} alt="/" />
+      </div>
     </div>
   );
 }
