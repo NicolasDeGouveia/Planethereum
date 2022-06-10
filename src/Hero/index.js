45c861cddef413e02a177586a06e989fb2f11f74
unknown <nicolas.degouveia@gmail.com>
2022-06-10 15:11:32 +0200

    fixing title problem

--- src/Hero/index.js
+++ src/Hero/index.js
@@ -3,7 +3,7 @@ import video from '../assets/video.mp4';
 
 export default function Hero() {
   return (
-    <div id='#home' className="hero">
+    <div className="hero">
       <div className="hero__container">
         <video src={video} autoPlay loop muted type='video/mp4' />
         <div className='hero__container__content'>
